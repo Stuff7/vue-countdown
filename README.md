a77e3ecd0bfece2ebad44c3f39d60e5943c8e266
Armando <cuentafrst@gmail.com>
2023-07-28 04:46:35 +0000

    Build static site.

--- README.md
+++
@@ -1,3 +0,0 @@
-# vue-countdown
-
-Just countdowns
--- env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="vite/client" />
-
-declare type Option<T> = T | null;
-declare type WithId<T> = T & { id: string };
-declare type MouseTouchEvent = MouseEvent | TouchEvent;
